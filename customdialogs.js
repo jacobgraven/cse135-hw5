a65f7e1a2580559303c5c9c0815292bd79e23d06
jacobgraven <jacobmgraven@gmail.com>
2023-03-01 23:06:51 -0800

    finish up dialog stuff

--- customdialogs.js
+++ customdialogs.js
@@ -1,23 +1,57 @@
 const alertDialog = document.getElementById("alert-dialog");
-// const confirmDialog = ...
-// const promptDialog = ...
+const confirmDialog = document.getElementById("confirm-dialog");
+const promptDialog = document.getElementById("prompt-dialog");
 
 const alertButton = document.getElementById("custom-alert");
 const confirmButton = document.getElementById("custom-confirm");
 const promptButton = document.getElementById("custom-prompt");
 
-/* Click Event Listeners */
+const output = document.getElementById("output");
+
+/* Event Listeners */
 
 alertButton.addEventListener("click", () => {
+    output.innerHTML = "";
     alertDialog.showModal();
-    const confirmChoice = document.getElementById("confirm-choice");
-    const cancelChoice = document.getElementById("confirm-choice");
 });
 
 confirmButton.addEventListener("click", () => {
-    // do something
+    output.innerHTML = "";
+    confirmDialog.showModal();
+
+    const confirmChoice = document.getElementById("confirm-choice");
+    const cancelChoice = document.getElementById("cancel-choice");
+    let returnValue;
+
+    confirmChoice.addEventListener("click", () => {
+        returnValue = true;
+        output.innerHTML = `Confirm result: ${returnValue}`;
+    });
+    cancelChoice.addEventListener("click", () => {
+        returnValue = false;
+        output.innerHTML = `Confirm result: ${returnValue}`;
+    });
 });
 
 promptButton.addEventListener("click", () => {
-    // do something
+    output.innerHTML = "";
+    promptDialog.showModal();
+
+    const submitChoice = document.getElementById("submit-choice");
+    const cancelChoice = document.getElementById("cancel-prompt");
+    const textField = document.getElementById("prompt-input");
+    let returnValue;
+
+    submitChoice.addEventListener("click", () => {
+        returnValue = textField.value;
+        if(returnValue != "") {
+            output.innerHTML = `Prompt result: ${returnValue}`;
+        } else {
+            output.innerHTML = `User didn't submit anything`;
+        }
+    });
+    cancelChoice.addEventListener("click", () => {
+        output.innerHTML = `User didn't submit anything`;
+    });
+
 });
